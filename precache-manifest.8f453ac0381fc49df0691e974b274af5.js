29e99000355cae342f61b5a3806208d0dd8f44b3
Tygrain <tgn.ukr@gmail.com>
2020-08-08 19:22:32 +0300

    обновлены файлы проекта todo-app

--- precache-manifest.8f453ac0381fc49df0691e974b274af5.js
+++ precache-manifest.8f453ac0381fc49df0691e974b274af5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dacb8d770ced8311c795b0324cfa34c9",
+    "revision": "9e515cf8a7792ce3d816a8d956e6a725",
     "url": "/index.html"
   },
   {
-    "revision": "8197c4af9678083a4013",
-    "url": "/static/css/main.6dff2281.chunk.css"
+    "revision": "8d00f82547d8610335ad",
+    "url": "/static/css/main.27d4c789.chunk.css"
   },
   {
     "revision": "74b43483311776275892",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.4a952419.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8197c4af9678083a4013",
-    "url": "/static/js/main.05f58ff3.chunk.js"
+    "revision": "8d00f82547d8610335ad",
+    "url": "/static/js/main.19e2148d.chunk.js"
   },
   {
     "revision": "73635f645c271e35ff15",
