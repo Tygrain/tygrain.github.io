3caf5bfc66040a9e4fe020fa70f3fa7408263fa9
Tygrain <tgn.ukr@gmail.com>
2020-08-08 16:54:23 +0300

    react app todo added

---
+++ precache-manifest.0c092bcd65cfeebeb0e465c90e264369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dacb8d770ced8311c795b0324cfa34c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8197c4af9678083a4013",
+    "url": "/static/css/main.6dff2281.chunk.css"
+  },
+  {
+    "revision": "74b43483311776275892",
+    "url": "/static/js/2.4a952419.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a952419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8197c4af9678083a4013",
+    "url": "/static/js/main.05f58ff3.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
