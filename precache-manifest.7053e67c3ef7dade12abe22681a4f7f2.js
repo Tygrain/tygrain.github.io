fdfa0d126db4589bd8845b4f6cc560225c9685aa
Tygrain <tgn.ukr@gmail.com>
2020-08-09 20:49:42 +0300

    обновлены файлы для todo-app

---
+++ precache-manifest.7053e67c3ef7dade12abe22681a4f7f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3c00a3f32be31f5fd271ff9e3fa9a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a805adbc276987d304d7",
+    "url": "/static/css/main.a521564d.chunk.css"
+  },
+  {
+    "revision": "74b43483311776275892",
+    "url": "/static/js/2.4a952419.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a952419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a805adbc276987d304d7",
+    "url": "/static/js/main.6ae573c2.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
